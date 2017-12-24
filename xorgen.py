92982aa3b4bf3374b475832106c1ae5c0bb36721
Chintan Soni <chintan.soni4@gmail.com>
2017-12-24 19:42:11 +0530

    Minor changes to XORgen

--- xorgen.py
+++ xorgen.py
@@ -3,22 +3,19 @@ Xor Dataset Generator : Python 3
 Author: Abhishek Munagekar
 '''
 import random
-
+import time
 #Parameters
-
-
-
 
 xor_num = 5
 nos_entry = 10000
-_seed = 3
-dumpfilename='dump.txt'
+_seed = int(time.time())
 
+dumpfilename='dump.txt'
 
 string = ['0','1']
 random.seed(_seed)
-
 dump = open(dumpfilename,'w')
+dump.write(str(nos_entry)+"\n")
 for i in range (nos_entry):
 	_input = [None]* xor_num
 	_output = 0
@@ -28,11 +25,3 @@ for i in range (nos_entry):
 		_output ^=_input[j]
 	dump.write(string[_output]+"\n")
 dump.close()
-
-
-
-
-
-
-
-
