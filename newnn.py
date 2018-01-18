582faf2367fc9792875f66e2e148553a462218cb
munagekar <avm.abhishek@gmail.com>
2018-01-18 20:27:01 +0530

    Code Cleanup

--- newnn.py
+++ newnn.py
@@ -8,12 +8,13 @@ Language: Python 3
 
 import itertools
 from functools import reduce
+import operator
 N_IN = 5
 
-
+#Generate the batch dataset here
 
 #A list of lists having N_IN elements all either 0 or 1
 xor_in = [list(i) for i in itertools.product([0, 1], repeat=N_IN)]
 #A list having 2^N lists each having xor of each input list in the list of lists
-xor_out = list(map(lambda i:[i],list(map(lambda x: (reduce(lambda i, j: int(i) ^ int(j),x)),xor_in))))
+xor_out = list(map(lambda x: [(reduce(operator.xor,x))],xor_in))
 
