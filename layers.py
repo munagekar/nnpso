dca1ff00862f9bf7cfeab379c7759c57c60b315b
munagekar <avm.abhishek@gmail.com>
2018-01-24 23:40:00 +0530

    Code Restyle as Python PEP8 & Added PSO init stuff

--- layers.py
+++ layers.py
@@ -1,41 +1,59 @@
 
 import tensorflow as tf
 
-#Activation Function
-def activate(input_layer,act = 'relu',name='activation'):
-	if act == None:
-		return input_layer
-	if act == 'relu':
-		return tf.nn.relu(input_layer,name)
-	if act =='sqr':
-		return tf.square(input_layer,name)
-	if act == 'sqr_sigmoid':
-		return tf.nn.sigmoid(tf.square(input_layer,name))
-	if act=='sigmoid':
-		return tf.nn.sigmoid(input_layer,name)
+# Activation Function
 
 
-#Fully connected cusom layer
-#Supported activation function types : None,relu,sqr,sqr_sigmoid,sigmoid
-def fc(input_tensor,n_output_units,scope,activation_fn= 'relu',uniform=False):
-	shape = [input_tensor.get_shape().as_list()[-1], n_output_units]
-	with tf.variable_scope(scope):
-		if uniform:
-			weights = tf.Variable(tf.random_uniform(
-					shape = shape,
-					dtype = tf.float32),
-				name='weights')
-		else:
-			weights = tf.Variable(tf.truncated_normal(
-                	shape=shape,
-                	mean=0.0,
-                	stddev=0.1,
-                	dtype=tf.float32),
+def activate(input_layer, act='relu', name='activation'):
+    if act == None:
+        return input_layer
+    if act == 'relu':
+        return tf.nn.relu(input_layer, name)
+    if act == 'sqr':
+        return tf.square(input_layer, name)
+    if act == 'sqr_sigmoid':
+        return tf.nn.sigmoid(tf.square(input_layer, name))
+    if act == 'sigmoid':
+        return tf.nn.sigmoid(input_layer, name)
+
+
+# Fully connected cusom layer for PSO
+# Supported activation function types : None,relu,sqr,sqr_sigmoid,sigmoid
+def fc(input_tensor, n_output_units, scope, activation_fn='relu', uniform=False):
+    shape = [input_tensor.get_shape().as_list()[-1], n_output_units]
+    # Use the Scope specified
+    with tf.variable_scope(scope):
+        # Init Weights
+        if uniform:
+            weights = tf.Variable(tf.random_uniform(
+                shape=shape,
+                dtype=tf.float32),
                 name='weights')
-		biases = tf.Variable(tf.zeros(shape=[n_output_units]),name='biases', dtype=tf.float32)
-		act = tf.matmul(input_tensor, weights) + biases
+        else:
+            weights = tf.Variable(tf.truncated_normal(
+                shape=shape,
+                mean=0.0,
+                stddev=0.1,
+                dtype=tf.float32),
+                name='weights')
+        # Init Biases
+        biases = tf.Variable(
+            tf.zeros(shape=[n_output_units]), name='biases', dtype=tf.float32)
+        # Particle Best
+        pbest_w = tf.Variable(weights.initialized_value(), name='pbest_w')
+        pbest_b = tf.Variable(biases.initialized_value(), name='pbest_b')
+        # Velocities
+        vel_weights = tf.Variable(tf.random_uniform(
+            shape=shape,
+            dtype=tf.float32),
+            name='vel_weights')
+        vel_biases = tf.Variable(tf.random_uniform(
+            shape=[n_output_units],
+            dtype=tf.float32),
+            name='vel_biases')
 
-		return activate(act,activation_fn),weights,biases
+        # Perform actual feedforward
+        act = tf.matmul(input_tensor, weights) + biases
 
-
-
+        # Activate And Return
+        return activate(act, activation_fn), weights, biases, pbest_w,pbest_b,vel_weights,vel_biases
--- newnn.py
+++ newnn.py
@@ -3,7 +3,7 @@ This file contains the new network definition
 Author: Abhishek Munagekar
 Language: Python 3
 '''
-#The number of values for xor
+# The number of values for xor
 
 
 import itertools
@@ -15,119 +15,117 @@ import random
 import layers
 
 
-#Dataset Generation for xor
+# Dataset Generation for xor
 
 N_IN = 5
 N_BATCHSIZE = 32
 
 
-#Parameters for pso
-#TODO: Add batch size and other properties
+# Parameters for pso
+# TODO: Add batch size and other properties
 N_PARTICLES = 4
 N_ITERATIONS = int(1e5)
 LEARNING_RATE = 0.001
 
 
-#Basic Neural Network Definition
-#Simple feedforward Network
+# Basic Neural Network Definition
+# Simple feedforward Network
 
-HIDDEN_LAYERS = [3,2]
-LAYERS = [N_IN]+HIDDEN_LAYERS+[1]
-print ('The Network Structure is',LAYERS)
-
-
-
-#Xorgenerator Function
-def xor_next_batch(batch_size,n_input):
-	batch_x = []
-	batch_y = []
-	for i in range(batch_size):
-		x=[]
-		y=[]
-		ans = 0
-		for j in range (n_input):
-			x.append(random.randint(0,1))
-			ans^=x[j]
-		y.append(ans)
-		batch_y.append(y)
-		batch_x.append(x)
-	return batch_x,batch_y
+HIDDEN_LAYERS = [3, 2]
+LAYERS = [N_IN] + HIDDEN_LAYERS + [1]
+print('The Network Structure is', LAYERS)
 
 
-#A list of lists having N_IN elements all either 0 or 1
+# Xorgenerator Function
+def xor_next_batch(batch_size, n_input):
+    batch_x = []
+    batch_y = []
+    for i in range(batch_size):
+        x = []
+        y = []
+        ans = 0
+        for j in range(n_input):
+            x.append(random.randint(0, 1))
+            ans ^= x[j]
+        y.append(ans)
+        batch_y.append(y)
+        batch_x.append(x)
+    return batch_x, batch_y
+
+
+# A list of lists having N_IN elements all either 0 or 1
 xor_in = [list(i) for i in itertools.product([0, 1], repeat=N_IN)]
-#A list having 2^N lists each having xor of each input list in the list of lists
-xor_out = list(map(lambda x: [(reduce(operator.xor,x))],xor_in))
+# A list having 2^N lists each having xor of each input list in the list
+# of lists
+xor_out = list(map(lambda x: [(reduce(operator.xor, x))], xor_in))
 
 
 net_in = tf.placeholder(dtype=tf.float32,
-						shape=[N_BATCHSIZE,N_IN],
-						name='net_in')
+                        shape=[N_BATCHSIZE, N_IN],
+                        name='net_in')
 
 label = tf.placeholder(dtype=tf.float32,
-					   shape=[N_BATCHSIZE,1],
-					   name='net_label')
+                       shape=[N_BATCHSIZE, 1],
+                       name='net_label')
 
-print ('Input & Label Set')
-print ('Starting to Build Network')
+print('Input & Label Set')
+print('Starting to Build Network')
 
-###MULTI-PARTICLE NEURAL NETS
+# MULTI-PARTICLE NEURAL NETS
 
 losses = []
 nets = []
-train_ops =[]
+train_ops = []
 
 
 for pno in range(N_PARTICLES):
-	net = net_in
+    net = net_in
 
-	for idx,num_neuron in enumerate(HIDDEN_LAYERS):
-		layer_scope = 'pno'+str(pno+1)+'fc'+str(idx+1)
-		net,w,b = layers.fc(input_tensor=net,
-						n_output_units =num_neuron,
-						activation_fn='sigmoid',
-						scope = layer_scope,
-						uniform = False)
+    for idx, num_neuron in enumerate(HIDDEN_LAYERS):
+        layer_scope = 'pno' + str(pno + 1) + 'fc' + str(idx + 1)
+        net, w, b, pw, pb, vw, vb = layers.fc(input_tensor=net,
+                                              n_output_units=num_neuron,
+                                              activation_fn='sigmoid',
+                                              scope=layer_scope,
+                                              uniform=False)
 
-	#Define loss for each of the particle nets
-	loss = tf.nn.l2_loss(net - label)
-	#Define an optimizer for the particle nets
-	optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-	train_op = optimizer.minimize(loss)
-	nets.append(net)
-	losses.append(loss)
-	train_ops.append(train_op)
-
+    # Define loss for each of the particle nets
+    loss = tf.nn.l2_loss(net - label)
+    # Define an optimizer for the particle nets
+    optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
+    train_op = optimizer.minimize(loss)
+    nets.append(net)
+    losses.append(loss)
+    train_ops.append(train_op)
 
 
 print('Network Build Successful')
 
 
-#Initialize the entire graph
+# Initialize the entire graph
 init = tf.global_variables_initializer()
-print ('Graph Init Successful')
+print('Graph Init Successful')
 
 
-#for var in tf.global_variables():
+# for var in tf.global_variables():
 #	print (var)
-
 
 
 req_list = losses + train_ops
 
 
-
 with tf.Session() as sess:
-	sess.run(init)
-	start_time = time.time()
-	for i in range(N_ITERATIONS):
-		#xor_in,xor_out = xor_next_batch(N_BATCHSIZE,N_IN)
-		dict_out= sess.run(req_list,feed_dict={net_in:xor_in,label:xor_out})
-		
-		if i%10000 == 0:
-			_losses = dict_out[:len(dict_out)//2]
-			print ('Losses:',_losses,'Iteration:',i)
+    sess.run(init)
+    start_time = time.time()
+    for i in range(N_ITERATIONS):
+        #xor_in,xor_out = xor_next_batch(N_BATCHSIZE,N_IN)
+        dict_out = sess.run(req_list, feed_dict={
+                            net_in: xor_in, label: xor_out})
 
-	end_time = time.time()
+        if i % 10000 == 0:
+            _losses = dict_out[:len(dict_out) // 2]
+            print('Losses:', _losses, 'Iteration:', i)
 
-	print('Total Time:',end_time-start_time)
+    end_time = time.time()
+
+    print('Total Time:', end_time - start_time)
