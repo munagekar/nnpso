b7a6fec835123de500f7911727705370b27e6c05
munagekar <avm.abhishek@gmail.com>
2018-01-26 22:30:22 +0530

    Added W&B Updates, Contains Mutabilility Bugs

--- newnn.py
+++ newnn.py
@@ -24,11 +24,14 @@ N_BATCHSIZE = 32
 # Parameters for pso
 # TODO: Add batch size and other properties
 N_PARTICLES = 1
-N_ITERATIONS = int(1e5)
+N_ITERATIONS = int(3)
 P_BEST_FACTOR = 2
 G_BEST_FACTOR = 2
 # Velocity Decay specifies the multiplier for the velocity update
-VELOCITY_DECAY = 1.0
+VELOCITY_DECAY = 0.5
+t_VELOCITY_DECAY = tf.constant(value=VELOCITY_DECAY,
+                               dtype=tf.float32,
+                               name='vel_decay')
 
 
 # Basic Neural Network Definition
@@ -84,6 +87,11 @@ pweights = []
 pbiases = []
 vweights = []
 vbiases = []
+ovbiases = []
+random_values = []
+bias_updates = []
+weight_updates = []
+nextvbs = []
 
 # Fixed Constant
 
@@ -91,7 +99,15 @@ vbiases = []
 # TODO:Parellized the following loop
 for pno in range(N_PARTICLES):
     net = net_in
-
+    # Define the parameters
+    w = None
+    b = None
+    pw = None
+    pb = None
+    vw = None
+    vb = None
+    gw = None
+    gb = None
     for idx, num_neuron in enumerate(LAYERS[1:]):
         layer_scope = 'pno' + str(pno + 1) + 'fc' + str(idx + 1)
         net, w, b, pw, pb, pf, vw, vb = layers.fc(input_tensor=net,
@@ -99,6 +115,10 @@ for pno in range(N_PARTICLES):
                                                   activation_fn='sigmoid',
                                                   scope=layer_scope,
                                                   uniform=True)
+        vweights.append(vw)
+        vbiases.append(vb)
+        weights.append(w)
+        biases.append(b)
         with tf.variable_scope(layer_scope, reuse=False):
             # Constants & Other Random Variables
             pbestrand = tf.Variable(tf.random_uniform(shape=[],
@@ -108,10 +128,13 @@ for pno in range(N_PARTICLES):
             gbestrand = tf.Variable(tf.random_uniform(shape=[],
                                                       maxval=G_BEST_FACTOR),
                                     name='gbestrand')
+            random_values.append(pbestrand)
+            random_values.append(gbestrand)
 
         # Multiply by the Velocity Decay
-        nextvw = tf.multiply(vw, VELOCITY_DECAY)
-        nextvb = tf.multiply(vb, VELOCITY_DECAY)
+        nextvw = tf.multiply(vw, t_VELOCITY_DECAY)
+        nextvb = tf.multiply(vb, t_VELOCITY_DECAY)
+
         # Differences between Particle Best & Current
         pdiffw = tf.multiply(tf.subtract(pw, w), pbestrand)
         pdiffb = tf.multiply(tf.subtract(pb, b), pbestrand)
@@ -128,10 +151,14 @@ for pno in range(N_PARTICLES):
         # Differences between Global Best & Current
         gdiffw = tf.multiply(tf.subtract(gw, w), gbestrand)
         gdiffb = tf.multiply(tf.subtract(gb, b), gbestrand)
-        vw = tf.add_n([nextvw, pdiffw, gdiffw])
-        vb = tf.add_n([nextvb, pdiffb, gdiffb])
-        w = tf.add(w, vw)
-        b = tf.add(b, vb)
+        vw = nextvw + pdiffw + gdiffw
+        vb = nextvb + pdiffb + gdiffb
+
+
+        weight_update = tf.assign(w, w + vw, validate_shape=True)
+        weight_updates.append(weight_update)
+        bias_update = tf.assign(b, b + vb, validate_shape=True)
+        bias_updates.append(bias_update)
 
     # Define loss for each of the particle nets
     loss = tf.nn.l2_loss(net - label)
@@ -151,22 +178,39 @@ print('Graph Init Successful:')
 for var in tf.global_variables():
     print(var)
 
+req_list = weights
+# Define the updates which are to be done before each iterations
+random_updates = [r.initializer for r in random_values]
+updates = weight_updates + bias_updates + random_updates
 
-req_list = losses
-
+print('Hello')
+for x, y in zip(vbiases, ovbiases):
+    if x is y:
+        print('Okay')
+    else:
+        print('Not Okay')
 
 with tf.Session() as sess:
     sess.run(init)
+
+    # Write The graph summary
+    summary_writer = tf.summary.FileWriter(
+        '/tmp/tf/logs', sess.graph_def)
     start_time = time.time()
     for i in range(N_ITERATIONS):
+        # Reinitialize the Random Values at each iteration
+        sess.run(updates)
+
         #xor_in,xor_out = xor_next_batch(N_BATCHSIZE,N_IN)
         dict_out = sess.run(req_list, feed_dict={
                             net_in: xor_in, label: xor_out})
 
-        if i % 10000 == 0:
-            _losses = dict_out
-            print('Losses:', _losses, 'Iteration:', i)
+        _losses = dict_out
+        print('Losses:', _losses, 'Iteration:', i)
 
     end_time = time.time()
+    # Close the writer
+    summary_writer.close()
 
     print('Total Time:', end_time - start_time)
+
