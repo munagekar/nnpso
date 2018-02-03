5836a0bfea13b359ba3c190a84aa2106a23703b4
munagekar <avm.abhishek@gmail.com>
2018-02-03 22:32:07 +0530

    Added Velocity Restriction Option

--- newnn.py
+++ newnn.py
@@ -24,12 +24,15 @@ N_BATCHSIZE = 32
 
 # Parameters for pso
 # TODO: Add batch size and other properties
-N_PARTICLES = 64
-N_ITERATIONS = int(1000)
-P_BEST_FACTOR = 0.05
-G_BEST_FACTOR = 0.05
+N_PARTICLES = 256
+N_ITERATIONS = int(40000)
+P_BEST_FACTOR = 2
+G_BEST_FACTOR = 2
+
 # Velocity Decay specifies the multiplier for the velocity update
-VELOCITY_DECAY = 0.999
+VELOCITY_DECAY = 1
+VELOCITY_RESTRICT = True
+MAX_VEL = 0.001
 t_VELOCITY_DECAY = tf.constant(value=VELOCITY_DECAY,
                                dtype=tf.float32,
                                name='vel_decay')
@@ -165,10 +168,16 @@ for pno in range(N_PARTICLES):
         # Differences between Global Best & Current
         gdiffw = tf.multiply(tf.subtract(gw, w), gbestrand)
         gdiffb = tf.multiply(tf.subtract(gb, b), gbestrand)
-
-        vweight_update = tf.assign(vw,
+        vweight_update = None
+        if VELOCITY_RESTRICT==False:
+            vweight_update = tf.assign(vw,
                                    tf.add_n([nextvw, pdiffw, gdiffw]),
                                    validate_shape=True)
+        else:
+            vweight_update = tf.assign(vw,
+                                   tf.maximum(tf.add_n([nextvw, pdiffw, gdiffw]),MAX_VEL),
+                                   validate_shape=True)            
+
         vweight_updates.append(vweight_update)
         vbias_update = tf.assign(vb,
                                  tf.add_n([nextvb, pdiffb, gdiffb]),
@@ -236,7 +245,7 @@ for var in tf.global_variables():
 random_updates = [r.initializer for r in random_values]
 updates = weight_updates + bias_updates + \
     random_updates + vbias_updates + vweight_updates + fit_updates
-req_list = losses, updates,gfit,gbiases
+req_list = losses, updates,gfit,gbiases,vweights,vbiases,gweights
 
 with tf.Session() as sess:
     sess.run(init)
@@ -248,12 +257,18 @@ with tf.Session() as sess:
         # Reinitialize the Random Values at each iteration
 
         # xor_in,xor_out = xor_next_batch(N_BATCHSIZE,N_IN)
-        dict_out, _ ,gfit,gbiases= sess.run(req_list, feed_dict={
+        dict_out, _ ,gfit,gbiases,vweights,vbiases,gweights= sess.run(req_list, feed_dict={
             net_in: xor_in, label: xor_out})
 
         _losses = dict_out
         print('Losses:', _losses, 'Iteration:', i)
-        print('Gfit:',gfit,':',gbiases)
+        print('Gfit:',gfit)
+        '''
+        print('gweights:',gweights)
+        print('gbiases:',gbiases)
+        print('vweights',vweights)
+        print('vbiases',vbiases)
+        '''
 
     end_time = time.time()
     # Close the writer
