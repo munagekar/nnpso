d3656b23caf0fe1e4858d102ced330d921bb8730
munagekar <avm.abhishek@gmail.com>
2018-02-12 21:48:40 +0530

    Try Catch Blocks added

--- parseutils.py
+++ parseutils.py
@@ -4,37 +4,36 @@
 
 import argparse
 
+
 # Integer Greater than zero
-# TODO: Add try catch block around the cast
-
-
 def intg0(value):
-    value = int(value)
-    if not isinstance(value, int):
+    try:
+        value = int(value)
+    except ValueError:
         raise argparse.ArgumentTypeError("%r not an Integer" % (value,))
     if value <= 0:
         raise argparse.ArgumentTypeError(
             "%r not a positive integer" % (value,))
     return value
 
+
 # Float in [0,1]
-
-
 def floatnorm(value):
-    value = float(value)
-    if not isinstance(value, float):
+    try:
+        value = float(value)
+    except ValueError:
         raise argparse.ArgumentTypeError("%r not a Float" % (value,))
     if value < 0 or value > 1:
         raise argparse.ArgumentTypeError(
             "%r not a positive Float <=1" % (value,))
     return value
 
+
 # Float >=0
-
-
 def pfloat(value):
-    value = float(value)
-    if not isinstance(value, float):
+    try:
+        value = float(value)
+    except ValueError:
         raise argparse.ArgumentError("%r not a Float" % (value,))
     if value <= 0:
         raise argparse.ArgumentError("%r not a positive Float" % (value,))
